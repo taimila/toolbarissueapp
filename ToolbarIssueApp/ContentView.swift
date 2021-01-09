5c352c04d3c6ea889945944b86bec51fc44e4efb
Lauri Taimila <lauri@taimila.com>
2021-01-09 13:33:38 +0200

    Initial Commit

---
+++ ToolbarIssueApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToolbarIssueApp
+//
+//  Created by Lauri Taimila on 9.1.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToolbarIssueApp/ToolbarIssueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarIssueAppApp.swift
+//  ToolbarIssueApp
+//
+//  Created by Lauri Taimila on 9.1.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarIssueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
